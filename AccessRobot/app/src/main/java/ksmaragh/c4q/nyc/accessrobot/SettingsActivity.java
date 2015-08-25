103adc67e36807ef17bb82afa6ed807d912ddfde
Sultan Keshwani <sultan_keshwani@timeinc.com>
2015-08-24 20:45:29 -0400

    added RadioGroup for InterfaceType (Bluetooth vs Serial cable)

--- AccessRobot/app/src/main/java/ksmaragh/c4q/nyc/accessrobot/SettingsActivity.java
+++ AccessRobot/app/src/main/java/ksmaragh/c4q/nyc/accessrobot/SettingsActivity.java
@@ -2,18 +2,43 @@ package ksmaragh.c4q.nyc.accessrobot;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 
-public class SettingsActivity extends ActionBarActivity {
+public class SettingsActivity extends AppCompatActivity {
+
+    RadioGroup rgInterfaceType;
+
+    RadioGroup.OnCheckedChangeListener interfaceTypeListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch (checkedId) {
+                case R.id.radio_bluetooth:
+                    Log.d("interface type", "bluetooth");
+                    break;
+                case R.id.radio_serial:
+                    Log.d("interface type", "serial cable");
+                    break;
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-    }
 
+        rgInterfaceType = (RadioGroup) findViewById(R.id.interface_type);
+        rgInterfaceType.setOnCheckedChangeListener(interfaceTypeListener);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
