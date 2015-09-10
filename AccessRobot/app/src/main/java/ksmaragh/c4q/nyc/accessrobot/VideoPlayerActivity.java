072f4a13f359341a00a3dac3aa1850039483fe97
Sultan Keshwani <sultan_keshwani@timeinc.com>
2015-09-10 06:30:49 -0400

    attempt to use surfaceView (video not playing)

--- AccessRobot/app/src/main/java/ksmaragh/c4q/nyc/accessrobot/VideoPlayerActivity.java
+++ AccessRobot/app/src/main/java/ksmaragh/c4q/nyc/accessrobot/VideoPlayerActivity.java
@@ -1,9 +1,11 @@
 package ksmaragh.c4q.nyc.accessrobot;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
@@ -12,12 +14,15 @@ import android.widget.MediaController;
 import android.widget.VideoView;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
     private static final String TAG = VideoPlayerActivity.class.getSimpleName();
 
     VideoView videoView = null;
+    MediaPlayer mediaPlayer = null;
 
     private void playMp4OnMediaView() {
         String videoUrl = "http://html5demos.com/assets/dizzy.mp4";
@@ -47,7 +52,38 @@ public class VideoPlayerActivity extends AppCompatActivity {
                         MediaController mediaController = new MediaController(VideoPlayerActivity.this);
                         mediaController.setAnchorView(videoView);
                         videoView.setMediaController(mediaController);
-                        mediaController.setAnchorView(videoView);
+                    }
+                });
+            }
+        });
+    }
+
+    private void playMp4OnSurfaceView() {
+        String videoUrl = "http://html5demos.com/assets/dizzy.mp4";
+        Uri videoUri = Uri.parse(videoUrl);
+        mediaPlayer = MediaPlayer.create(this, videoUri);
+        final SurfaceView sf = (SurfaceView) findViewById(R.id.surface_view);
+
+        final SurfaceHolder surfaceHolder = sf.getHolder();
+        //surfaceHolder.addCallback(this); requires implementing SurfaceHolder.Callback
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+
+                surfaceHolder.setFixedSize(mp.getVideoWidth(), mp.getVideoHeight());
+                mp.start();
+
+                mp.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+                        /*
+                         * add media controller
+                         */
+                        MediaController mediaController = new MediaController(VideoPlayerActivity.this);
+                        mediaController.setAnchorView(sf);
+                        videoView.setMediaController(mediaController);
+
                     }
                 });
             }
@@ -59,7 +95,7 @@ public class VideoPlayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
         videoView = (VideoView) findViewById(R.id.video_view);
-        playMp4OnMediaView();
+        playMp4OnSurfaceView();
 
     }
 
@@ -82,6 +118,10 @@ public class VideoPlayerActivity extends AppCompatActivity {
         if (videoView.isPlaying()) {
             videoView.stopPlayback();
         }
+
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
     }
 
     @Override
@@ -99,5 +139,21 @@ public class VideoPlayerActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // implements SurfaceHolder.Callback
+    // @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    // @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    // @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 }
