35ee31767daa83c7afc00f8f2df65498b5d34fa4
Sultan Keshwani <sultan_keshwani@timeinc.com>
2015-09-10 06:14:04 -0400

    play mp4 from url. This uses VideoView

--- AccessRobot/app/src/main/java/ksmaragh/c4q/nyc/accessrobot/VideoPlayerActivity.java
+++ AccessRobot/app/src/main/java/ksmaragh/c4q/nyc/accessrobot/VideoPlayerActivity.java
@@ -1,16 +1,87 @@
 package ksmaragh.c4q.nyc.accessrobot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.io.IOException;
 
 public class VideoPlayerActivity extends AppCompatActivity {
+
+    private static final String TAG = VideoPlayerActivity.class.getSimpleName();
+
+    VideoView videoView = null;
+
+    private void playMp4OnMediaView() {
+        String videoUrl = "http://html5demos.com/assets/dizzy.mp4";
+        Uri videoUri = Uri.parse(videoUrl);
+        videoView.setVideoURI(videoUri);
+//
+//        //2a) optional but good:
+//        //Use a media controller so that you can scroll the video contents
+//        //and also to pause, start the video.
+
+//        MediaController mediaController = new MediaController(VideoPlayerActivity.this);
+//        mediaController.setAnchorView(videoView);
+//        videoView.setMediaController(mediaController);
+
+        // 2b) prep the video and then set the controller, so you can position controls
+
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+                /*
+                 * add media controller
+                 */
+                        MediaController mediaController = new MediaController(VideoPlayerActivity.this);
+                        mediaController.setAnchorView(videoView);
+                        videoView.setMediaController(mediaController);
+                        mediaController.setAnchorView(videoView);
+                    }
+                });
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
+        videoView = (VideoView) findViewById(R.id.video_view);
+        playMp4OnMediaView();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        videoView.start();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        videoView.pause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (videoView.isPlaying()) {
+            videoView.stopPlayback();
+        }
     }
 
     @Override
